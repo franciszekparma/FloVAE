491bef34feb3b32ed23e9be819acb40bdb461756
FP <franciszek.parma@gmail.com>
2026-05-01 08:34:16 +0200

    Fixed critical bugs

--- c.py
+++ c.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+device = 'cuda' if torch.cuda.is_available() else 'mps'
 
 class DoubleDownConv(nn.Module):
   def __init__(self, c_in, c_out):
@@ -38,43 +39,70 @@ class DoubleDownConv(nn.Module):
 
 
 class DoubleUpConv(nn.Module):
-  def __init__(self, c_in, c_out):
+  def __init__(self, c_in, c_out, last=False):
     super().__init__()
     
-    self.double_conv = nn.Sequential(
-      nn.Conv2d(
-        in_channels=c_in,
-        out_channels=c_out,
-        kernel_size=3,
-        padding=1,
-        stride=1,
-        bias=True
-      ),
-      nn.GroupNorm(8, c_out),
-      nn.GELU(),
-      
-      nn.Conv2d(
-        in_channels=c_out,
-        out_channels=c_out,
-        kernel_size=3,
-        padding=1,
-        stride=1,
-        bias=True
-      ),
-      nn.GroupNorm(8, c_out),
-      nn.GELU(),
-      
-      nn.ConvTranspose2d(
-        in_channels=c_out,
-        out_channels=c_out,
-        kernel_size=2,
-        stride=2,
-        padding=0,
-        bias=True
-      ),
-      nn.GroupNorm(8, c_out)
-    )
-    
+    if not last:
+      self.double_conv = nn.Sequential(
+        nn.Conv2d(
+          in_channels=c_in,
+          out_channels=c_out,
+          kernel_size=3,
+          padding=1,
+          stride=1,
+          bias=True
+        ),
+        nn.GroupNorm(8, c_out),
+        nn.GELU(),
+        
+        nn.Conv2d(
+          in_channels=c_out,
+          out_channels=c_out,
+          kernel_size=3,
+          padding=1,
+          stride=1,
+          bias=True
+        ),
+        nn.GroupNorm(8, c_out),
+        nn.GELU(),
+        
+        nn.Upsample(scale_factor=2, mode='bilinear'),
+        nn.GroupNorm(8, c_out)
+      )
+    else:
+      self.double_conv = nn.Sequential(
+          nn.Conv2d(
+            in_channels=c_in,
+            out_channels=c_out,
+            kernel_size=3,
+            padding=1,
+            stride=1,
+            bias=True
+          ),
+          nn.GroupNorm(3, 3),
+          nn.GELU(),
+          
+          nn.Conv2d(
+            in_channels=c_out,
+            out_channels=c_out,
+            kernel_size=3,
+            padding=1,
+            stride=1,
+            bias=True
+          ),
+          nn.GroupNorm(3, 3),
+          nn.GELU(),
+          
+          nn.ConvTranspose2d(
+            in_channels=c_out,
+            out_channels=c_out,
+            kernel_size=2,
+            stride=2,
+            padding=0,
+            bias=True
+          ),
+          nn.GroupNorm(3, 3)
+        )
     
   def forward(self, X):
     return self.double_conv(X)
@@ -82,61 +110,176 @@ class DoubleUpConv(nn.Module):
 
 
 class VAE(nn.Module):
-  def __init__(self, num_layers=4, hid_c_dim=256, z_dim=128):
+  def __init__(self, num_layers=4, hid_c_dim=256, z_dim=64):
     super().__init__()
     
     self.encoder = nn.Sequential(
       DoubleDownConv(3, 32),
       DoubleDownConv(32, 64),
       DoubleDownConv(64, 128),
-      DoubleDownConv(128, 256),
-      DoubleDownConv(256, 512, last=True)
+      DoubleDownConv(128, 256)
+    )
+    
+    self.last_encoder_layer = nn.Sequential(
+      nn.GroupNorm(8, 256),
+      
+      nn.Conv2d(
+        in_channels=256,
+        out_channels=256,
+        kernel_size=3,
+        padding=1,
+        stride=1,
+        bias=False
+      ),
+      nn.GroupNorm(8, 256),
+      nn.GELU(),
+      
+      nn.Conv2d(
+        in_channels=256,
+        out_channels=256,
+        kernel_size=3,
+        padding=1,
+        stride=1,
+        bias=True
+      )
     )
     
     self.hid_2_mean = nn.Sequential(
-      nn.Linear(512 * 4**2, z_dim*4),
+      nn.Conv2d(
+        in_channels=256,
+        out_channels=128,
+        kernel_size=3,
+        padding=1,
+        stride=1,
+        bias=True
+      ),
+      nn.GroupNorm(8, 128),
       nn.GELU(),
-      nn.LayerNorm(z_dim*4),
       
-      nn.Linear(z_dim*4, z_dim)
+      nn.AdaptiveAvgPool2d((4, 4)),
+      nn.Flatten(),
+      
+      nn.Linear(2048, z_dim * 4),
+      nn.LayerNorm(z_dim * 4),
+      nn.GELU(),
+      
+      nn.Linear(z_dim * 4, z_dim)
     )
     
-    self.hid_2_std = nn.Sequential(
-      nn.Linear(512 * 4**2, z_dim*4),
+    self.hid_2_logvar = nn.Sequential(
+      nn.Conv2d(
+        in_channels=256,
+        out_channels=128,
+        kernel_size=3,
+        padding=1,
+        stride=1,
+        bias=True
+      ),
+      nn.GroupNorm(8, 128),
       nn.GELU(),
-      nn.LayerNorm(z_dim*4),
       
-      nn.Linear(z_dim*4, z_dim)
+      nn.AdaptiveAvgPool2d((4, 4)),
+      nn.Flatten(),
+      
+      nn.Linear(2048, z_dim * 4),
+      nn.LayerNorm(z_dim * 4),
+      nn.GELU(),
+      
+      nn.Linear(z_dim * 4, z_dim)
     )
     
     self.z_2_hid = nn.Sequential(
-      nn.Linear(z_dim, 128//2**5)
+      nn.Linear(z_dim, (128//2**4)**2 * 128),
+      nn.Unflatten(1, (128, 8, 8)),
+      
+      nn.Conv2d(
+        in_channels=128,
+        out_channels=256,
+        kernel_size=3,
+        stride=1,
+        padding=1,
+        bias=True
+      ),
+      nn.GroupNorm(8, 256),
+      nn.GELU()
     )
     
     self.decoder = nn.Sequential(
-      DoubleUpConv(512, 256),
       DoubleUpConv(256, 128),
       DoubleUpConv(128, 64),
       DoubleUpConv(64, 32),
       DoubleUpConv(32, 3, last=True)
     )
     
+    self.last_decoder_layer = nn.Sequential(
+      nn.GroupNorm(1, 3),
+      
+      nn.Conv2d(
+        in_channels=3,
+        out_channels=3,
+        kernel_size=3,
+        padding=1,
+        stride=1,
+        bias=False
+      ),
+      nn.GroupNorm(1, 3),
+      nn.GELU(),
+      
+      nn.Conv2d(
+        in_channels=3,
+        out_channels=3,
+        kernel_size=3,
+        padding=1,
+        stride=1,
+        bias=True
+      )
+    )
+    
   
   def encode(self, img):
     B = img.shape[0]
     
-    h = self.decoder(img).view(B, -1)
-    mean, std = self.hid_2_mean(h), self.hid_2_std(h)
-    
-    return mean, std    
+    h = self.last_encoder_layer(self.encoder(img))
+    mean, logvar = self.hid_2_mean(h), self.hid_2_logvar(h)
+    return mean, logvar    
   
   def decode(self, z):
-    h = self.z_2_hid(z)
-    out = self.decoder(z)
+    h = self.z_2_hid(z).view(-1, 256, 128//2**4, 128//2**4)
+    out = self.last_decoder_layer(self.decoder(h))
     
     return F.tanh(out)
   
   def forward(self, X):
-    mean, std = self.encode(X)
+    mean, logvar = self.encode(X)
+    std = torch.exp(logvar * 0.5)
     
     z_reparam = mean + std * torch.randn_like(mean)
+    out = self.decode(z_reparam)
+    
+    return out, mean, std
+  
+
+def count_params(model):
+    total_params = 0
+
+    print("\n=== PARAMETER COUNT PER LAYER ===\n")
+
+    for name, module in model.named_modules():
+        params = sum(p.numel() for p in module.parameters(recurse=False))
+
+        if params > 0:
+            print(f"{name:50s} -> {params:,}")
+            total_params += params
+
+    print("\n==============================")
+    print(f"TOTAL PARAMETERS: {total_params:,}")
+    print("==============================\n")
+
+
+if __name__ == '__main__':
+    model = VAE().to(device)
+
+    count_params(model)
+
+    x = torch.randn((32, 3, 128, 128)).to(device)
+    out, mean, std = model(x)
